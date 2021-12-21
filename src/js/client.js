2a30d748cc56ca43f5bc5a7cfd7119d4e82ebc9d
Rafał Kazik <kazik.raf@gmail.com>
2021-12-21 23:10:11 +0100

    client almost done

--- src/js/client.js
+++ src/js/client.js
@@ -5,12 +5,14 @@ import "./../css/client.css";
 const excursionsUrl = "http://localhost:3000/excursions";
 const ordersUrl = "http://localhost:3000/orders";
 const basket = [];
+const finalPriceBasket = [];
 
 document.addEventListener("DOMContentLoaded", init);
 
 function init() {
   loadExcurisons();
   addExcursionToBasket();
+  // sumUpFinalPrice();
 }
 
 function loadExcurisons() {
@@ -135,7 +137,7 @@ function addExcursionToBasket() {
 
       //
       //
-      // FINAL PRICE FOR ALL //
+      // FINAL PRICE FOR ALL PER ONE EXCURSION //
       //
       //
       function finalPriceForAll() {
@@ -144,40 +146,164 @@ function addExcursionToBasket() {
 
       console.log(`Cena za CALE zamowienie: ${finalPriceForAll()}`);
 
-      // ***
-      // Show excursion in the basket
-      // ***
-      function showExcursionOrderInBasket() {
+      finalPriceBasket.push(finalPriceForAll());
+
+      // VALIDATE FORM
+
+      const childrenAmountInput = targetChildPriceParent.querySelector(
+        "input[name='children']"
+      );
+
+      const adultsAmountInput = targetAdultPriceParent.querySelector(
+        "input[name='adults']"
+      );
+
+      const childrenAmountInputValue = childrenAmountInput.value;
+
+      const adultsAmountInputValue = adultsAmountInput.value;
+
+      if (
+        !isNaN(parseInt(childrenAmountInputValue)) &&
+        !isNaN(parseInt(adultsAmountInputValue))
+      ) {
         // ***
-        // find and get excursion name
+        // Show excursion in the basket
         // ***
-        const targetParent = targetEl.parentElement;
-        const targetChildPriceParent = targetParent.previousElementSibling;
-        const targetAdultPriceParent =
-          targetChildPriceParent.previousElementSibling;
-        const targetExcursionDataForm = targetAdultPriceParent.parentElement;
-        const targetExcursionHeader =
-          targetExcursionDataForm.previousElementSibling;
+        function showExcursionOrderInBasket() {
+          // ***
+          // find and get excursion name
+          // ***
+          const targetParent = targetEl.parentElement;
+          const targetChildPriceParent = targetParent.previousElementSibling;
+          const targetAdultPriceParent =
+            targetChildPriceParent.previousElementSibling;
+          const targetExcursionDataForm = targetAdultPriceParent.parentElement;
+          const targetExcursionHeader =
+            targetExcursionDataForm.previousElementSibling;
 
-        const targetExcursionName =
-          targetExcursionHeader.querySelector(".excursions__title").innerText;
+          // DATA TO VALIDATE FORM
+          const childrenAmount = targetChildPriceParent.querySelector(
+            "input[name='children']"
+          ).value;
 
-        // EXCURSION NAME BASKET
-        const excursionNameInBasket = (document.querySelector(
-          ".summary__name"
-        ).innerText = targetExcursionName);
+          const adultsAmount = targetAdultPriceParent.querySelector(
+            "input[name='adults']"
+          ).value;
+          //
 
-        // EXCURSION DESCRIPTION
-        const excursionDescriptionInBasket = (document.querySelector(
-          ".summary__prices"
-        ).innerText = `dorośli: ${getAmountOfAdults()} x ${checkTargetExcursionAdultPrice()} PLN, dzieci: ${getAmountOfChildren()} x ${checkTargetExcursionChildPrice()} PLN`);
+          const targetExcursionName =
+            targetExcursionHeader.querySelector(".excursions__title").innerText;
 
-        const basketExcursion = document.querySelector(".summary__item");
-        const cloneBasketExcursion = basketExcursion.cloneNode(true);
-        cloneBasketExcursion.classList.remove("summary__item--prototype");
-        basketList.appendChild(cloneBasketExcursion);
+          // EXCURSION NAME BASKET
+          const excursionNameInBasket = (document.querySelector(
+            ".summary__name"
+          ).innerText = targetExcursionName);
+
+          // EXCURSION DESCRIPTION
+          const excursionDescriptionInBasket = (document.querySelector(
+            ".summary__prices"
+          ).innerText = `dorośli: ${getAmountOfAdults()} x ${checkTargetExcursionAdultPrice()} PLN, dzieci: ${getAmountOfChildren()} x ${checkTargetExcursionChildPrice()} PLN`);
+
+          // EXCURSION PRICE IN BASKET
+          const summaryPricePerExcursion = (document.querySelector(
+            ".summary__total-price"
+          ).innerText = `${finalPriceForAll()} PLN`);
+
+          // Clone and append basket item
+          const basketExcursion = document.querySelector(".summary__item");
+          const cloneBasketExcursion = basketExcursion.cloneNode(true);
+          cloneBasketExcursion.classList.remove("summary__item--prototype");
+          basketList.appendChild(cloneBasketExcursion);
+        }
+        showExcursionOrderInBasket();
       }
-      showExcursionOrderInBasket();
+      if (!isNaN(parseInt(childrenAmountInputValue))) {
+        childrenAmountInput.classList.remove("excursions__field-input--error");
+      }
+      if (!isNaN(parseInt(adultsAmountInputValue))) {
+        adultsAmountInput.classList.remove("excursions__field-input--error");
+      }
+      if (isNaN(parseInt(childrenAmountInputValue))) {
+        childrenAmountInput.classList.add("excursions__field-input--error");
+      }
+      if (isNaN(parseInt(adultsAmountInputValue))) {
+        adultsAmountInput.classList.add("excursions__field-input--error");
+      }
+
+      function sumUpFinalPrice() {
+        const finalValuePrice = finalPriceBasket.reduce((a, b) => a + b);
+        const totalOrderPrice = (document.querySelector(
+          ".order__total-price-value"
+        ).innerText = `${finalValuePrice} PLN`);
+      }
+      sumUpFinalPrice();
     }
   });
 }
+
+const orderSubmitBtn = document.querySelector(
+  ".order__field-input[value='zamawiam']"
+);
+
+// function sumUpFinalPrice() {
+//   const basketList = document.querySelector(".panel__summary");
+//   const basketListItems = basketList.querySelectorAll(".summary__item");
+
+//   basketListItems.forEach((item) => {
+//     if (!item.classList.contains("summary__item--prototype")) {
+//       const excursionInBasketPrice = item
+//         .querySelector(".summary__total-price")
+//         .innerText.slice(0, -4);
+
+//       return excursionInBasketPrice;
+//       // finalPriceBasket.push(parseInt(excursionInBasketPrice));
+
+//       // totalOrderPrice.innerText = finalPriceBasket.reduce((a, b) => a + b);
+
+//       // return parseInt(excursionInBasketPrice);
+//     }
+//   });
+// }
+
+function showNamesOfExcursionsInBasket(e) {
+  e.preventDefault();
+
+  const basketList = document.querySelector(".panel__summary");
+  const basketListItems = basketList.querySelectorAll(".summary__item");
+
+  basketListItems.forEach((item) => {
+    if (!item.classList.contains("summary__item--prototype")) {
+      const excursionInBasketName =
+        item.querySelector(".summary__name").innerText;
+
+      const excursionInBasketPrice = item.querySelector(
+        ".summary__total-price"
+      ).innerText;
+
+      const excursionInBasketDescription =
+        item.querySelector(".summary__prices").innerText;
+
+      basket.push(
+        `Miasto: ${excursionInBasketName}, cena za całość: ${excursionInBasketPrice} (${excursionInBasketDescription})`
+      );
+    }
+  });
+  const data = {
+    fullBasket: basket,
+    fullPrice: finalPriceBasket.reduce((a, b) => a + b),
+  };
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: { "Content-Type": "application/json" },
+  };
+
+  fetch(ordersUrl, options)
+    .then((resp) => console.log(resp))
+    .catch((err) => console.error(err))
+    .finally(loadExcurisons);
+}
+
+orderSubmitBtn.addEventListener("click", showNamesOfExcursionsInBasket);
+// orderSubmitBtn.addEventListener("click", sumUpFinalPrice);
